641d2fd7f07fbf6a7ff8a59467a31654c541a7a4
AquaVirtual <aquavirtual.aqua@gmail.com>
2023-01-09 13:57:16 +0700

    First Commit

---
+++ Assets/Scripts/Main/GetTime/DisplayTime.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTime : MonoBehaviour {
+
+    public GameObject theDisplay;
+    public int hour;
+    public int minutes;
+    public int seconds;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        hour = System.DateTime.Now.Hour;
+        minutes = System.DateTime.Now.Minute;
+        seconds = System.DateTime.Now.Second;
+        theDisplay.GetComponent<Text>().text = "" + hour + ":" + minutes + ":" + seconds;
+    }
+}
